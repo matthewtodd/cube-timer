1a6a7ea5a3cb1b1cf175d5d3295e6f58567c0f0b
Matthew Todd <matthew@matthewtodd.org>
2025-02-16 16:23:01 -0500

    Initial Commit

---
+++ Cube Timer/Cube_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cube_TimerApp.swift
+//  Cube Timer
+//
+//  Created by Matthew Todd on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Cube_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cube TimerUITests/Cube_TimerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Cube_TimerUITestsLaunchTests.swift
+//  Cube TimerUITests
+//
+//  Created by Matthew Todd on 2/16/25.
+//
+
+import XCTest
+
+final class Cube_TimerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
