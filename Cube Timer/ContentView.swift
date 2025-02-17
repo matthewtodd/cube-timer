3159cdec715dfb19988f7cf277ef7c065490012b
Matthew Todd <matthew@matthewtodd.org>
2025-02-17 10:38:24 -0500

    Working!

--- Cube Timer/ContentView.swift
+++ Cube Timer/ContentView.swift
@@ -6,17 +6,66 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    enum TimerState {
+        case zero
+        case running
+        case stopped
+        
+        func tick(_ content: ContentView) {
+            if self == .running {
+                content.tick()
+            }
         }
-        .padding()
+        
+        func advance(_  content: ContentView) {
+            switch self {
+            case .zero:
+                content.start()
+            case .running:
+                content.stop()
+            case .stopped:
+                content.reset()
+            }
+        }
     }
+    
+    @State private var state: TimerState = .zero
+    @State private var startTime = Date()
+    @State private var duration: Duration = .zero
+    private let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    private let format: Duration.TimeFormatStyle = .time(pattern: .minuteSecond(padMinuteToLength: 2, fractionalSecondsLength: 3))
+    
+    func start() {
+        state = .running
+        startTime = Date()
+    }
+    
+    func tick() {
+        duration = .seconds(-startTime.timeIntervalSinceNow)
+    }
+    
+    func stop() {
+        state = .stopped
+    }
+    
+    func reset() {
+        state = .zero
+        duration = .zero
+    }
+    
+    var body: some View {
+        Text(format.format(duration))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(UIColor.systemBackground))
+            .font(.system(size: 72, design: .monospaced))
+            .onReceive(timer) { _ in state.tick(self) }
+            .onTapGesture { state.advance(self) }
+    }
+    
 }
 
 #Preview {
